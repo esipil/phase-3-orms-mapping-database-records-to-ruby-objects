11ff419c28593b08c28d2bc4ba42db37aa3dad8a
Timothy <lemayiant4@gmail.com>
2022-10-31 15:18:57 +0300

    completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
 class Song
 
   attr_accessor :name, :album, :id
+  
+  @@all
 
   def initialize(name:, album:, id: nil)
     @id = id
@@ -48,5 +50,30 @@ class Song
     song = Song.new(name: name, album: album)
     song.save
   end
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], album: row[2])
 
+  end
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM songs
+    WHERE name = ?
+    LIMIT 1
+  SQL
+
+  DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+    end.first
+  end
 end
